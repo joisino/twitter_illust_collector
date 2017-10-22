6b0e891b5b35ff59dc1a0f1d7f1b9a88d587defb
joisino <joisinoo@gmail.com>
2017-10-22 15:54:02 +0900

    add face place checker

---
+++ check.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os
+import cv2
+import config
+
+def filter_illust():
+    # move to the repository
+    os.chdir( os.path.dirname(os.path.abspath(__file__)) )
+    
+    files = os.listdir("img")
+
+    for f in files:
+        filename = "img/" + f
+        faceCascade = cv2.CascadeClassifier(config.CASCADE_FILE)
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow("Faces found", img)
+        cv2.waitKey(0)
+
+if __name__ == '__main__':
+    filter_illust()
